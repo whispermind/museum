916ac5461ddb356759674ac406ba4c97ce5a2146
whispermind <h4vefun.dieyoung@gmail.com>
2021-09-27 22:22:34 +0300

    mount slider to video area

--- museum/scripts/script.js
+++ museum/scripts/script.js
@@ -1,5 +1,5 @@
 let progress = document.querySelectorAll('.player__range');
 progress.forEach((elem) => elem.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%)`
 }));
