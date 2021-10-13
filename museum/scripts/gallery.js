4cb6f1e61862873065c85fa86c8ecbadc4774346
whispermind <h4vefun.dieyoung@gmail.com>
2021-10-13 14:20:17 +0300

    feat: add appearing animation to the gallery section

--- museum/scripts/gallery.js
+++ museum/scripts/gallery.js
@@ -5,9 +5,38 @@ export default function randomizer() {
     let elem = document.createElement('img');
     elem.src = `./assets/pictures/gallery/${i}.webp`;
     elem.alt = 'gallery picture';
-    elem.classList.add('gallery-image')
+    elem.classList.add('gallery-image');
     images.push(elem);
   }
   images.sort(() => Math.random() - 0.5);
   images.forEach((elem) => container.append(elem));
+  applyAnimation();
+};
+function applyAnimation() {
+  window.addEventListener('scroll', debounce(animationHandler));
+  let galleryImages = document.querySelectorAll('.gallery-image');
+  function animationHandler(event) {
+    galleryImages.forEach((img) => {
+      let coords = img.getBoundingClientRect();
+      if (coords.top < window.innerHeight - (img.height / 4)) {
+        img.classList.add('gallery-image-active');
+      };
+      if (coords.top < 0) img.classList.remove('gallery-image-active');
+    });
+  }
+}
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 };
